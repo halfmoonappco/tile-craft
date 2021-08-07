bfe90977331a2841f076ea38ecfeb48bcdceb260
halfmoonappco <halfmooncoapps@gmail.com>
2021-08-06 21:52:58 -0400

    Basic Prototype 0.0.1
    Added basic level player movement (just for testing.) Player can recognize being on start tile and generate first set of secondary tiles.

---
+++ Tile-Craft/Assets/Scripts/Char_move1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char_move1 : MonoBehaviour
+{
+
+    Vector3 moveVector = Vector3.zero;
+    CharacterController characterController;
+
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector.x = Input.GetAxis("Horizontal") * moveSpeed;
+        moveVector.z = Input.GetAxis("Vertical") * moveSpeed;
+
+        characterController.SimpleMove(moveVector * Time.deltaTime);
+        characterController.Move(moveVector * Time.deltaTime);
+    }
+}
